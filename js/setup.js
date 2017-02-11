e6fc533047c7f602d18e1bcf8af0ef58f2d75d60
zbralex <5998971@gmail.com>
2017-02-11 13:19:13 +0300

    добавил табиндексы, формы открываются/закрываются кнопками, DRY выполню след пушем

--- js/setup.js
+++ js/setup.js
@@ -2,13 +2,35 @@
 
 var setupOpen = document.querySelector('.setup-open');
 var setupOverlay = document.querySelector('.overlay');
+var setupClose = document.querySelector('.setup-close');
+var ESCAPE_KEY_CODE = 27;
+var ENTER_KEY_CODE = 13;
+// открываем окно
 setupOpen.addEventListener('click', function () {
   setupOverlay.classList.remove('invisible');
 });
-var setupClose = document.querySelector('.setup-close');
+// добавляем кейкод на клавишу enter
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEY_CODE) {
+    setupOverlay.classList.remove('invisible');
+    document.addEventListener('keydown', function(evt){
+      if (evt.keyCode === ESCAPE_KEY_CODE){ // если окно открыто, то при нажатии на esc окно закрывается
+        setupOverlay.classList.add('invisible');
+      }
+    })
+  }
+});
+// закрываем окно
 setupClose.addEventListener('click', function () {
   setupOverlay.classList.add('invisible');
 });
+// добавляем кейкод на клавишу escape
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ESCAPE_KEY_CODE) {
+    setupOverlay.classList.add('invisible');
+  }
+});
+// маг
 var wizard = document.querySelector('#wizard');
 var wizardFrench = document.querySelector('#wizard-coat');
 var wizardFrenchColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
